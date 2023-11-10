c006837516297bb83e6b29f2026af24772c111c4
root <root@kali.(none)>
2023-11-10 09:11:03 -0500

    all

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -15,8 +15,8 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*p;
 
 	p = malloc(count * size);
-	if (p == 0)
-		return (p);
+	if (!p)
+		return (NULL);
 	ft_bzero(p, count * size);
 	return (p);
 }
