a660b711e7284398ed4790c678eb563ced3b1d32
Oleg Shklyarov <oshklyar@2C2.42angouleme.fr>
2024-03-25 14:58:34 +0100

    all

--- Minitalk/client.c
+++ Minitalk/client.c
@@ -1,4 +1,53 @@
-int main (void)
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "libft/libft.h"
+
+static void signal_action(int sig)
 {
+    static int  receive = 0;
+    if (sig == SIGUSR1)
+        receive ++;
+    else
+    {
+        ft_putnbr_fd(receive, 1);
+        ft_putchar_fd('\n', 1);
+        exit(0);
+    }
+}
+
+void    ft_kill(int pid, char *str)
+{
+    int i;
+    char    c;
+
+    while (*str)
+    {
+        i = 8;
+        c = *str ++;
+        while (i --)
+        {
+            if (c >> i & 1)
+                kill(pid, SIGUSR2);
+            else
+                kill(pid, SIGUSR2);
+            usleep(100);
+        }
+    }
+    i = 8;
+    while (i --)
+    {
+        kill(pid, SIGUSR1);
+        usleep(100);
+    }
+}
+
+int main (int argc, char **argv)
+{
+    if (argc != 3 || !ft_strlen(argv[2]))
+        return(1);
+    ft_putstr_fd("SENT:     ", 1);
+    
+
     return(0);
 }
--- Minitalk/server.c
+++ Minitalk/server.c
@@ -1,4 +1,17 @@
+#include <signal.h>
+#include <unistd.h>
+#include "libft/libft.h"
+
+static void	action ();
+
 int	main(void)
 {
+	struct sigaction	s_sigaction;
+	ft_putstr_fd("SERVER PID:     ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putchar_fd('\n', 1);
+	s_sigaction.sa_sigaction = action;
+
+	
 	return (0);
 }
