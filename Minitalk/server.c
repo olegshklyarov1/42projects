41bd262956ee008f824879bf7a8c80868e0c95aa
oleg shklyarov <oleg.shklyarov@gmail.com>
2024-10-21 13:08:12 +0200

    all

--- Minitalk/server.c
+++ Minitalk/server.c
@@ -1,45 +1,79 @@
 #include <signal.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include "libft/libft.h"
 
-static void	action(int sig, siginfo_t *info, void *context)
+typedef struct s_message
 {
-	static int				i = 0;
-	static pid_t			client_pid = 0;
-	static unsigned char	c = 0;
+	unsigned char	current_char;
+	int	bit_position;
+	pid_t	client_pid;
+	size_t	total_chars;
+}	t_message;
+
+static t_message g_msg = {0 , 0, 0, 0};
+
+static void	handle_message(int sig, siginfo_t *info, void *context)
+{
 
 	(void)context;
-	if (!client_pid)
-		client_pid = info->si_pid;
-	c |= (sig == SIGUSR2);
-	if (++i == 8)
+
+	if (!g_msg.client_pid)
 	{
-		i = 0;
-		if (!c)
-		{
-			kill(client_pid, SIGUSR2);
-			client_pid = 0;
-			return ;
-		}
-		ft_putchar_fd(c, 1);
-		c = 0;
-		kill(client_pid, SIGUSR1);
+		g_msg.client_pid = info->si_pid;
+		ft_putstr_fd("\nNew client connection [", 1);
+		ft_putnbr_fd(g.msg.client_pid, 1);
+		ft_putstr_fd("]\n", 1);
 	}
-	else
-		c <<= 1;
+	g_msg.current_char = (g_msg.current_char << 1) | (sig == SIGUSR2);
+	g_msg.bit_position++;
+	if (g_msg.bit_position == 8)
+	{
+		if (!g_msg.current_char)
+		{
+			ft_putstr_fd("\nMessage received - ", 1);
+			ft_putnbr_fd(g_msg.total_chars, 1);
+			ft_putstr_fd(" characters\n", 1);
+			kill(g_msg.client_pid, SIGUSR2);
+			g_msg = (t_message){0, 0, 0, 0};
+			return;
+		}
+		ft_putchar_fd(g_msg.current_char, 1);
+		g_msg.total_chars++;
+		g_msg.current_char = 0;
+		g_msg.bit_position = 0;
+		if (kill(g_msg.client_pid, SIGUSR1) == -1)
+		{
+			ft_putstr_fd("\nError: disconnected client\n", 2);
+			g_msg = (t_message){0, 0, 0, 0};
+			return;
+		}
+	}
+}
+
+static void	setup_signals(void)
+{
+	struct sigaction sa;
+	sa.sa_sigaction = handle_message;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
+	{
+		ft_putstr_fd("ERROR : Impossible to configure signals\n", 2);
+		exit(1);
+	}
 }
 
 int	main(void)
 {
 	struct sigaction	s_sigaction;
 
-	ft_putstr_fd("Server PID: ", 1);
+	ft_putstr_fd("\n === Minitalk Server ===\nServer PID: ", 1);
 	ft_putnbr_fd(getpid(), 1);
 	ft_putchar_fd('\n', 1);
-	s_sigaction.sa_sigaction = action;
-	s_sigaction.sa_flags = SA_SIGINFO;
-	sigaction(SIGUSR1, &s_sigaction, 0);
-	sigaction(SIGUSR2, &s_sigaction, 0);
+	setup_signals();
 	while (1)
 		pause();
 	return (0);
